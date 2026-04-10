f5ec6c11b0f81e45710efeefcf3330a56ca94c69
Fazal <boyswild104@gmail.com>
2026-04-10 09:53:32 +0530

    initialize the project and installed required dependencies : 1.tailwindcss 2.gsap

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CenterDiv from './components/common/CenterDiv'
+
+const App = () => {
+  return (
+    <div className='w-full h-full '>
+        <CenterDiv/>
+    </div>
+  )
+}
+
+export default App
